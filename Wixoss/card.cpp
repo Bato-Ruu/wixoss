77334c96a9f04b452c79e40461fd1380fcf32903
Ranigad <li.w.charles@gmail.com>
2015-04-06 13:07:58 -0700

    Directory reference is now absolute based on the current path Commented card.h

--- Wixoss/card.cpp
+++ Wixoss/card.cpp
@@ -1,55 +1,54 @@
-#include "card.h"
-//#include <iostream>
 #include <QDebug>
 #include <QtSql>
 #include <QDir>
-#include <QTextStream>
-using namespace std;
+#include "card.h"
 
 const QString Card::DATABASE = "cards.db";
 
 Card::Card(int id)
 {
-    QTextStream cout(stdout);
+    qDebug() << "----- Card Constructor -----" << endl;
     this->id = id;
 
-    QString databasePath = QDir().toNativeSeparators(QDir().currentPath() + "/debug/" + Card::DATABASE);
-        cout << databasePath << endl;
+    QString databasePath = QDir().toNativeSeparators(QDir().currentPath() + "/" + Card::DATABASE);
+
     // Makes a new database
     QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
     db.setDatabaseName(databasePath);
+
     // Checks if database can't be opened
     if(!db.open())
-        cout << "Database could not be found" << endl;
+        qDebug() << "Database could not be found" << endl
+                 << "Database path: " << databasePath << endl;
+
 
     // Query database
-
-    //db.setDatabaseName(QDir::toNativeSeparators(Card::DATABASE));
 
     QSqlQuery query(db);
 
     // Gets the card name and image from the database cards
-    QString query_string;// + QString::number(id) + ";";
-    query.exec("SELECT Name, Image FROM Cards WHERE Id = " + QString::number(id) +  ";");
-    cout << query.executedQuery() << endl;
+    QString query_string = "SELECT Name, Image FROM Cards WHERE Id = " + QString::number(id) +  ";";
+    query.exec(query_string);
+
+    //qDebug() << "Executed Query: " << query.executedQuery() << endl;
+
     query.first();  // Checks and sets the query to the first row
 
     // Initializing the values of the card
     name = query.value(0).toString();
-    imageFile = QDir().currentPath() + "/debug/setimages/" + query.value(1).toString();
+    imageFile = QDir().currentPath() + "/setimages/" + query.value(1).toString();
     isWhite = query.value(2).toInt() == 1;
     pixmap = QPixmap(QDir().toNativeSeparators(imageFile));
 
-    cout << QDir().toNativeSeparators(imageFile) << endl;
-    /*** Error Checking ***/
     // Checks if the imageFile exists
-
     if (pixmap.isNull())
-        cout << "Image could not be found" << endl;
-//    pixmapItem = new QGraphicsPixmapItem(pixmap);
+        qDebug() << "Image could not be found" << endl
+                 << "Image File Directory: " << QDir().toNativeSeparators(imageFile) << endl;
+        //pixmapItem = new QGraphicsPixmapItem(pixmap);
 
-    cout << "Hi from card " << name.toUtf8().data() << "." << endl;
+    //qDebug() << "Hi from card " << name.toUtf8().data() << "." << endl;
     db.close();
+    qDebug() << "----- End of Card Constructor -----" << endl;
 }
 
 QGraphicsPixmapItem* Card::getPixmapItem() {
