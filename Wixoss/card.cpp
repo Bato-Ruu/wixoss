480263ca795171204b625a9b0ecf32db32bb2ca0
Ranigad <li.w.charles@gmail.com>
2015-04-06 12:44:21 -0700

    Added commenting and personal local functionality.

--- Wixoss/card.cpp
+++ Wixoss/card.cpp
@@ -1,29 +1,52 @@
 #include "card.h"
-#include <stdio.h>
+//#include <iostream>
 #include <QtSql>
 #include <QDir>
+#include <QTextStream>
+using namespace std;
 
 const char* Card::DATABASE = "cards.db";
 
 Card::Card(int id)
 {
+    QTextStream cout(stdout);
     this->id = id;
+
+    QString databasePath = QDir().toNativeSeparators(QDir().currentPath() + "/debug/" + Card::DATABASE);
+        cout << databasePath << endl;
+    // Makes a new database
     QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
-    db.setDatabaseName(QDir().toNativeSeparators(Card::DATABASE));
+    db.setDatabaseName(databasePath);
+    // Checks if database can't be opened
     if(!db.open())
-        printf("Ded");
+        cout << "Database could not be found" << endl;
+
+    // Query database
     QSqlQuery query(db);
-    QString query_string = "SELECT Name, Image FROM Cards WHERE Id = " + QString::number(id) + ";";
-    query.exec(query_string);
-    query.first();
+
+    // Gets the card name and image from the database cards
+    QString query_string;// + QString::number(id) + ";";
+    query.exec("SELECT Name, Image FROM Cards WHERE Id = " + QString::number(id) +  ";");
+    cout << query.executedQuery() << endl;
+    query.first();  // Checks and sets the query to the first row
+
+    // Initializing the values of the card
     name = query.value(0).toString();
-    imageFile = "setimages/" + query.value(1).toString();
+    imageFile = QDir().currentPath() + "/debug/setimages/" + query.value(1).toString();
     isWhite = query.value(2).toInt() == 1;
     pixmap = QPixmap(QDir().toNativeSeparators(imageFile));
+
+    cout << QDir().toNativeSeparators(imageFile) << endl;
+    /*** Error Checking ***/
+    // Checks if the imageFile exists
+
     if (pixmap.isNull())
-        printf("lol fucked\n");
+        cout << "Image could not be found" << endl;
 //    pixmapItem = new QGraphicsPixmapItem(pixmap);
-    printf("Hi from card %s.\n", name.toUtf8().data());
+
+    cout << "Hi from card " << name.toUtf8().data() << "." << endl;
+    db.close();
+
 }
 
 QGraphicsPixmapItem* Card::getPixmapItem() {
